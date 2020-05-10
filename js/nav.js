062987ca87fac90b3f1d6559f55b0a4ea63170ff
Meghan Prestemon <meghanprestemon@Meghans-MBP.fios-router.home>
2020-05-10 19:44:16 -0400

    Add jQuery

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded')
-  document.getElementById('hamburger-icon').addEventListener('click', activateOffCanvasNav)
+function activateOffCanvasNav() {
+  var hamburger = document.querySelector('#hamburger-icon');
+  var offCanvasWrapper = document.querySelector('#off-canvas-wrapper');
+
+  hamburger.classList.toggle('active');
+  offCanvasWrapper.classList.toggle('responsive');
+}
+
+function activateSubmenu() {
+   this.classList.toggle('dropdown');
+}
+
+
+
+
+
+
+
+$(document).ready(function () {
+  $('#hamburger-icon').click(activateOffCanvasNav);
+  $('.has-submenu').click(activateSubmenu);
 });
 
-function activateOffCanvasNav(e) {
-  console.log('in off canvas function')
-  e.currentTarget.classList.toggle('active');
-  console.log('hamburger active')
 
-  // var mainMenu = document.getElementById("nav");
-  //
-  // if (mainMenu.className === "nav") {
-  //   mainMenu.className += " off-canvas";
-  // } else {
-  //   mainMenu.className = "nav";
-  // }
-  //
-  // if (mainMenu.classList.contains("off-canvas")) {
-  //   document.getElementById("off-canvas-wrapper").style.height = "100%";
-  // } else {
-  //   document.getElementById("off-canvas-wrapper").style.height = "auto";
-  // }
 
-}
-
-function activateSubmenu(e) {
-   e.classList.toggle("dropdown");
-}
+// function activateOffCanvasNav(e) {
+//   console.log('in off canvas function')
+//   e.currentTarget.classList.toggle('active');
+//   console.log('hamburger active')
+//
+//   // var mainMenu = document.getElementById("nav");
+//   //
+//   // if (mainMenu.className === "nav") {
+//   //   mainMenu.className += " off-canvas";
+//   // } else {
+//   //   mainMenu.className = "nav";
+//   // }
+//   //
+//   // if (mainMenu.classList.contains("off-canvas")) {
+//   //   document.getElementById("off-canvas-wrapper").style.height = "100%";
+//   // } else {
+//   //   document.getElementById("off-canvas-wrapper").style.height = "auto";
+//   // }
+//
+// }
+//
+// function activateSubmenu(e) {
+//    e.classList.toggle("dropdown");
+// }
+//
+// document.getElementById('hamburger-icon').addEventListener('click', activateOffCanvasNav(getElementById('hamburger-icon')));
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('loaded')
+// });
