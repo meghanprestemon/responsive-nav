6fcecb165e1b584aa1c78425174421ea58edb1fe
Meghan Prestemon <meghanprestemon@Meghans-MBP.fios-router.home>
2020-05-10 20:26:41 -0400

    Add small screen style

--- js/nav.js
+++ js/nav.js
@@ -10,46 +10,9 @@ function activateSubmenu() {
    this.classList.toggle('dropdown');
 }
 
-
-
-
-
-
+// INITIALIZERS 
 
 $(document).ready(function () {
   $('#hamburger-icon').click(activateOffCanvasNav);
   $('.has-submenu').click(activateSubmenu);
 });
-
-
-
-// function activateOffCanvasNav(e) {
-//   console.log('in off canvas function')
-//   e.currentTarget.classList.toggle('active');
-//   console.log('hamburger active')
-//
-//   // var mainMenu = document.getElementById("nav");
-//   //
-//   // if (mainMenu.className === "nav") {
-//   //   mainMenu.className += " off-canvas";
-//   // } else {
-//   //   mainMenu.className = "nav";
-//   // }
-//   //
-//   // if (mainMenu.classList.contains("off-canvas")) {
-//   //   document.getElementById("off-canvas-wrapper").style.height = "100%";
-//   // } else {
-//   //   document.getElementById("off-canvas-wrapper").style.height = "auto";
-//   // }
-//
-// }
-//
-// function activateSubmenu(e) {
-//    e.classList.toggle("dropdown");
-// }
-//
-// document.getElementById('hamburger-icon').addEventListener('click', activateOffCanvasNav(getElementById('hamburger-icon')));
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('loaded')
-// });
